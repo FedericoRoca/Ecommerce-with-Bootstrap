d86f01a8fb8f2783babc1348f3420b2b067d2bbf
Federico Roca <roca.fedee@gmail.com>
2021-02-22 16:53:26 -0300

    images-cart and first javascrit part

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-document.getElementById("cart-info").addEventListener("click", function () {
+//show cart
+(function () {
   const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
-});
+  const cartInfo = document.getElementById("cart-info");
+  cartInfo.addEventListener("click", function () {
+    cart.classList.toggle("show-cart");
+  });
+})();
+// add items to the cart
+(function () {
+  const cartBtn = document.querySelectorAll(".store-item-icon");
+  cartBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+      // console.log(event.target);
+      if (event.target.parentElement.classList.contains("store-item-icon")) {
+        console.log(event.target.parentElement.previousElementSibling.src);
+        // let pos = fulPath;
+      }
+    });
+  });
+})();
